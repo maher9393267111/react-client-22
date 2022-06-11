4b77a643b593287ccc084682f3e92dd9a1122616
maher <gomemahero.com>
2022-06-11 13:49:05 +0300

    register start work with firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,7 +1,10 @@
 
-import firebase from 'firebase'
-
-const firebaseConfig =  firebase.initializeApp({
+import { initializeApp,getApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+//import firebase from "firebase/app";
+const firebaseConfig =  initializeApp({
     apiKey: "AIzaSyC4qfODc1T-EIDV3JL-DsQ_nBuNWr0ne5A",
     authDomain: "react-auth-da2f5.firebaseapp.com",
     projectId: "react-auth-da2f5",
@@ -10,10 +13,32 @@ const firebaseConfig =  firebase.initializeApp({
     appId: "1:622489962584:web:f911abf1b8c1b7b49ea22d"
 });
 
+
+let firebaseApp;
+try {
+    firebaseApp = getApp();
+} catch (e) {
+  firebaseApp = initializeApp(firebaseConfig);
+}
+
+
+
+
 // Initialize Firebase
-const db = firebaseConfig.firestore()
+//export const app = initializeApp(firebaseConfig);
+export const app = firebaseApp
+export const storage = getStorage();
+export const db = getFirestore();
+export const auth = getAuth();
 
-const auth = firebase.auth()
 
-const storage = firebase.storage();
-export { db, auth,storage }
+
+
+
+// Initialize Firebase
+// const db = firebaseConfig.firestore()
+
+// const auth = firebase.auth()
+
+// const storage = firebase.storage();
+// export { db, auth,storage }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,25 +1,81 @@
-import React from 'react'
-import firebase from 'firebase'
+import React, { useState, useEffect,useRef } from "react";
+import { auth } from "../../firebase";
+import { toast } from "react-toastify";
+import { useSelector } from "redux";
+import { sendEmailVerification } from 'firebase/auth';
+
+import { useAuth } from '../../context/AuthContext';
+//import { sendEmailVerification } from 'firebase/auth';
+
+const Register = ({ history }) => {
+  
+    const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  const [isRegister, setIsRegister] = useState(false);
+  const {
+    modal,
+    setModal,
+    signUp,
+    login,
+    loginWithGoogle,
+    setAlert,
+    setLoading,
+    currentUser
+  } = useAuth();
 
 
-import { auth } from '../../firebase/index'
-// import { Button } from '@material-ui'
-// 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-function SignIn() {
-    function signInWithGoogle() {
-        const provider = new firebase.auth.GoogleAuthProvider()
-        auth.signInWithPopup(provider)
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
 
-        // find current user data in firebase
-        const useer = auth.currentUser
-        console.log(useer.displayName)
-    }
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center' }}>
-            <button style={{ padding: '30px', fontSize: '20px', borderRadius: '0', fontWeight: '600' }} onClick={signInWithGoogle}>Sign In With Google</button>
+
+    await signUp(email, password);
+    console.log('current auth user', currentUser)
+
+  }
+
+
+
+
+
+  return (
+    <div className="container p-5">
+
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          
+
+<div>
+    <h1>email</h1>
+    <input type="email" ref ={emailRef} />
+</div>
+
+
+
+<div>
+
+<h1>password</h1>
+<input type="password" ref= {passwordRef} />
+
+</div>
+
+
+<button
+onClick={handleSubmit}
+
+> register</button>
+
+
+
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default SignIn
+export default Register;
