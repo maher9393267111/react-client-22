6d4ed73e43df9c6817dcf8ac9c190cfd9afda373
maher <gomemahero.com>
2022-06-12 04:25:09 +0300

    crypto api project

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import "./App.css";
 import axios from "axios";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
+import CoinPage from "./pages/CoinPage";
 import { useState, useEffect, useRef } from "react";
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { Route, Routes } from 'react-router-dom';
+import Signin from "./pages/Signin";
+import Account from "./pages/Account";
 function App() {
   const checkref = useRef(null);
 
@@ -40,15 +44,16 @@ useEffect(() => {
   <Navbar />
    <Routes>
           <Route path='/' element={<Home coins={coins} />} />
-          {/* <Route path='/signin' element={<Signin />} /> */}
+          <Route path='/coin/:coinId' element={<CoinPage />}></Route>
+          <Route path='/signin' element={<Signin />} />
           <Route path='/signup' element={<Signup />} />
-          {/* <Route path='/account' element={<Account />} />
-          <Route path='/coin/:coinId' element={<CoinPage />}>
-            <Route path=':coinId' /> */}
+            <Route path='/account' element={< Account/>} />  
+       
+            {/* <Route path=':coinId' /> */} 
           {/* </Route> */}
         </Routes> 
 
-{/* <Footer /> */}
+<Footer />
 
 
   </div>
